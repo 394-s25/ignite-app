ffbdb9322e06ce2b71914aa617cafb2bf24ee3ec
ananyabhtia <ananyabhtia@gmail.com>
2025-04-06 23:28:29 -0500

    skills/prefs tables added

--- src/db/firebaseService.js
+++ src/db/firebaseService.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, set, push, get, query, orderByChild, equalTo } from "firebase/database";
 import SHA256 from "crypto-js/sha256";
+// import { L } from "vitest/dist/chunks/reporters.d.CqBhtcTq";
 
 const firebaseConfig = {
     databaseURL: "https://ignite-app-red-default-rtdb.firebaseio.com"
@@ -10,7 +11,36 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
-export async function writeUserData(name, email) {
+// initialize skills and preferences dictionaries
+// sample skills and preferences, longer list will be made later
+const skillsArr = ['JavaScript', 'Logo Design', 'Product Management'];
+const preferencesArr = ['1-10 Employees', '10-50 Employees', 'Healthcare Sector'];
+const skillsRef = ref(db, 'skills/');
+const prefsRef = ref(db, 'preferences/');
+
+skillsArr.forEach((skill) => {
+    const newSkillRef = push(skillsRef);
+    set(newSkillRef, { skill: skill })
+    .then(() => {
+        console.log(`${skill} written successfully.`);
+    })
+    .catch((error) => {
+        console.error(`Could not write ${skill} to database: ${error}`);
+    });
+});
+
+preferencesArr.forEach((pref) => {
+    const newPrefRef = push(prefsRef);
+    set(newPrefRef, { pref: pref })
+    .then(() => {
+        console.log(`${pref} written successfully.`);
+    })
+    .catch((error) => {
+        console.error(`Could not write ${pref} to database: ${error}`);
+    });
+});
+
+export async function userFirstWrite(name, email) {
     const hashedEmail = SHA256(email).toString();
     return set(ref(db, 'users/' + hashedEmail), {
         username: name,
@@ -22,6 +52,20 @@ export async function writeUserData(name, email) {
     .catch((error) => {
         console.error("Error writing data: ", error);
     });
+}
+
+export async function userSecondWrite(userid, major, bio, skills, preferences) {
+    const userRef = ref(db, 'users/' + userid)
+    return set(userRef, {
+        major: major,
+        bio: bio
+    })
+    .then(() => {
+        console.log("User second write successful.")
+    })
+    .catch((error) => {
+        console.error(`Error writing data: ${error}`)
+    })
 }
 
 export async function writeCompanyData(name, introduction, email, matchedSkills) {
