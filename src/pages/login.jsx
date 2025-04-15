e401b748fd395275c25e0ce0c60bc2c5ce095091
YangchichengShen <78356476+YangchichengShen@users.noreply.github.com>
2025-04-14 22:43:13 -0500

    ✨ Add reroute function to google signin

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,11 +1,19 @@
 import { handleGoogleLogin } from "../db/firebaseAuth";
 
+const onSuccessRoute = async () => {
+    const user = await handleGoogleLogin()
+    if (user) {
+        // Redirect to the appropriate page based on user type
+        window.location.href = "/studentswipe";
+    }
+}
+
 const LoginPage = () => {
     return (
         <div className="flex flex-col items-center justify-center h-screen">
             <h1 className="text-3xl font-bold mb-4">Welcome to Ignite -- Northwestern Garage Job Matching Website</h1>
             <button
-            onClick={handleGoogleLogin}
+            onClick={onSuccessRoute}
             className="bg-purple-600 text-white px-4 py-2 rounded"
             >
             Sign in with Google
